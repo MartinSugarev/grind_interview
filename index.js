02d86d7c635a2d35b2463ac10821cbcb9653dc42
Martin <mdsugarev@gmail.com>
2022-04-24 17:24:04 +0300

    rasp

---
+++ index.js
@@ -0,0 +1,15 @@
+let openNavBtn = document.body.querySelector(".fa-solid.fa-arrow-down-short-wide")
+let closeNavBtn = document.body.querySelector(".close")
+openNavBtn.addEventListener("click", () => {
+    let navbar = document.getElementById("navbar");
+    closeNavBtn.classList.add("active")
+    openNavBtn.classList.add("active")
+    navbar.classList.add("active")
+})
+
+closeNavBtn.addEventListener("click", () => {
+    let navbar = document.getElementById("navbar");
+    navbar.classList.remove("active");
+    closeNavBtn.classList.remove("active")
+    openNavBtn.classList.remove("active")
+})
